33b577c48f2b01c44ec1012163571d92e2a8c205
sarahAldrees <439200791@student.ksu.edu.sa>
2021-11-06 19:39:40 +0300

    change some elements in log in and sign up

--- lib/widget/auth/auth_form.dart
+++ lib/widget/auth/auth_form.dart
@@ -8,6 +8,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
+//
 class AuthForm extends StatefulWidget {
   AuthForm(
     this.submitFn,
