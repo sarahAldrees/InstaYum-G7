d78a6630888f7763e20f081eaa54e33f0ab3306a
GhaidaaAlomran <alomranGhaida@gmail.com>
2021-11-22 14:27:55 +0300

    Follow the previous one, but a simple problem has been solved in data.dart

--- lib/data.dart
+++ lib/data.dart
@@ -6,19 +6,59 @@ import 'model/cookbook.dart';
 
 class Data extends StatefulWidget {
   @override
-  _DataState createState() => _DataState();
+  DataState createState() => DataState();
 }
 
-// const Cookbook_Data = const [
-//   Cookbook(
-//     id: 'c1',
-//     cookbookName: 'Default cookbook',
-//     imageURLCookbook:
-//         'https://lacuisinedegeraldine.fr/wp-content/uploads/2021/06/Pancakes-04483-2-scaled.jpg',
-//   ),
-// ];
+var Cookbook_Data = [
+  Cookbook(
+    id: 'c1',
+    cookbookName: 'Default cookbook',
+    imageURLCookbook:
+        'https://lacuisinedegeraldine.fr/wp-content/uploads/2021/06/Pancakes-04483-2-scaled.jpg',
+  ),
+];
+var Recipes_Data = const [
+  Recipe(
+    // id: 'r1',
+    recipeName: 'Pancakes',
+    imageURL:
+        'https://lacuisinedegeraldine.fr/wp-content/uploads/2021/06/Pancakes-04483-2-scaled.jpg',
+    typeOfMeal: "breakfast",
+    category: 'c1',
+    cuisine: "indian",
+    ingredients: [
+      '4 Tomatoes',
+      '1 Tablespoon of Olive Oil',
+      '1 Onion',
+    ],
+    dirctions: [
+      'Tenderize the veal to about 2–4mm, and salt on both sides.',
+      'On a flat plate, stir the eggs briefly with a fork.',
+      'Lightly coat the cutlets in flour then dip into the egg, and finally, coat in breadcrumbs.',
+    ],
+  ),
+  Recipe(
+    // id: 'r1',
+    recipeName: 'Pancakes',
+    imageURL:
+        'https://lacuisinedegeraldine.fr/wp-content/uploads/2021/06/Pancakes-04483-2-scaled.jpg',
+    typeOfMeal: "breakfast",
+    category: 'c1',
+    cuisine: "indian",
+    ingredients: [
+      '4 Tomatoes',
+      '1 Tablespoon of Olive Oil',
+      '1 Onion',
+    ],
+    dirctions: [
+      'Tenderize the veal to about 2–4mm, and salt on both sides.',
+      'On a flat plate, stir the eggs briefly with a fork.',
+      'Lightly coat the cutlets in flour then dip into the egg, and finally, coat in breadcrumbs.',
+    ],
+  )
+];
 
-class _DataState extends State<Data> {
+class DataState extends State<Data> {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
 //  db.collection("users").get().then((querySnapshot) => {
@@ -36,8 +76,11 @@ class _DataState extends State<Data> {
         .collection("recpies")
         .get()
         .then((querySnapshot) {
-      querySnapshot.docs
-          .forEach((doc) => {recpiesList.add(Recipe(id: doc.id))});
+      querySnapshot.docs.forEach((doc) => {
+            recpiesList.add(Recipe(
+              id: doc.id,
+            ))
+          });
     });
     //     .snapshots()
     //     .listen((userData) {
@@ -47,47 +90,6 @@ class _DataState extends State<Data> {
     //   });
     // });
   }
-
-  var Recipes_Data = const [
-    Recipe(
-      // id: 'r1',
-      recipeName: 'Pancakes',
-      imageURL:
-          'https://lacuisinedegeraldine.fr/wp-content/uploads/2021/06/Pancakes-04483-2-scaled.jpg',
-      typeOfMeal: "breakfast",
-      category: 'c1',
-      cuisine: "indian",
-      ingredients: [
-        '4 Tomatoes',
-        '1 Tablespoon of Olive Oil',
-        '1 Onion',
-      ],
-      dirctions: [
-        'Tenderize the veal to about 2–4mm, and salt on both sides.',
-        'On a flat plate, stir the eggs briefly with a fork.',
-        'Lightly coat the cutlets in flour then dip into the egg, and finally, coat in breadcrumbs.',
-      ],
-    ),
-    Recipe(
-      // id: 'r1',
-      recipeName: 'Pancakes',
-      imageURL:
-          'https://lacuisinedegeraldine.fr/wp-content/uploads/2021/06/Pancakes-04483-2-scaled.jpg',
-      typeOfMeal: "breakfast",
-      category: 'c1',
-      cuisine: "indian",
-      ingredients: [
-        '4 Tomatoes',
-        '1 Tablespoon of Olive Oil',
-        '1 Onion',
-      ],
-      dirctions: [
-        'Tenderize the veal to about 2–4mm, and salt on both sides.',
-        'On a flat plate, stir the eggs briefly with a fork.',
-        'Lightly coat the cutlets in flour then dip into the egg, and finally, coat in breadcrumbs.',
-      ],
-    )
-  ];
 
   @override
   Widget build(BuildContext context) {
