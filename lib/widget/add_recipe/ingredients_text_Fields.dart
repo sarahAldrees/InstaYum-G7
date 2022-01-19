2970dff12766142b942f0fad90b24b7680b71f8e
ghadaALtamimi <439200969@student.ksu.edu.sa>
2022-01-19 22:27:49 +0300

    first submit of spech to text

--- lib/widget/add_recipe/ingredients_text_Fields.dart
+++ lib/widget/add_recipe/ingredients_text_Fields.dart
@@ -1,5 +1,7 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 import 'package:instayum1/widget/add_recipe/add_recipe_page.dart';
+import 'package:instayum1/widget/add_recipe/speech_to_text_API.dart';
 
 class IngredientsTextFields extends StatefulWidget {
   final int index;
@@ -11,7 +13,8 @@ class IngredientsTextFields extends StatefulWidget {
 
 class _IngredientsTextFieldsState extends State<IngredientsTextFields> {
   TextEditingController _ingredientController;
-
+  String text = "";
+  bool isListening = false;
   @override
   void initState() {
     super.initState();
@@ -28,6 +31,20 @@ class _IngredientsTextFieldsState extends State<IngredientsTextFields> {
     return TextFormField(
       controller: _ingredientController,
       decoration: InputDecoration(
+          //============================
+          suffixIcon: AvatarGlow(
+            animate: isListening,
+            endRadius: 25,
+            glowColor: Colors.orange,
+            child: IconButton(
+              onPressed: toggleRecording,
+              icon: Icon(
+                isListening ? Icons.mic : Icons.mic_none,
+                color: Color(0xFFeb6d44),
+              ),
+            ),
+          ),
+          //---------------------
           hintText: 'Enter an ingredient'), //errorText: _errorText
       onChanged: (value) {
         addRecipe.userIngredients[widget.index] = value;
@@ -40,4 +57,10 @@ class _IngredientsTextFieldsState extends State<IngredientsTextFields> {
       },
     );
   }
+
+  Future toggleRecording() => SpeechApi.toggleRecording(
+      onResult: (text) => setState(() => this.text = text),
+      onListening: (isListening) {
+        setState(() => this.isListening = isListening);
+      });
 }
