ab8c052995dfade3ae2cbfd8d0b2b2adc14116e2
GhaidaaAlomran <alomranGhaida@gmail.com>
2022-01-26 02:02:44 +0300

    training the chatbot

--- lib/widget/discover/discover_page.dart
+++ lib/widget/discover/discover_page.dart
@@ -3,7 +3,6 @@ import 'package:instayum1/widget/discover/dialog_flow.dart';
 import 'package:instayum1/widget/recipe_view/my_recipes_screen.dart';
 import 'package:flutter/cupertino.dart';
 import '../recipe_view/my_recipes_screen.dart';
-import 'chatbot_chat.dart';
 
 //import 'package:flutter/material.dart';
 
--- lib/widget/discover/message.dart
+++ lib/widget/discover/message.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:instayum1/main_pages.dart';
+import 'package:instayum1/widget/meal_plan/meal_title.dart';
+import 'package:instayum1/widget/recipe_view/recipe_view.dart';
 
 class Messages extends StatelessWidget {
   Messages({this.text, this.name, this.type});
@@ -6,42 +9,172 @@ class Messages extends StatelessWidget {
   final String text;
   final String name;
   final bool type;
+  final List<String> ingredients = ["milk"];
+  final List<String> dirctions = ["1"];
+  final List<String> dircimageUrlst = [
+    "  https://firebasestorage.googleapis.com/v0/b/instayum-f7a34.appspot.com/o/recpie_image%2FdefaultRecipeImage.png?alt=media&token=f12725db-646b-4692-9ccf-131a99667e43"
+  ];
+  final image = AssetImage("assets/images/defaultRecipeImage.png");
+
+  List<List<String>> mealInformation = [
+    ["hello", "Hello"],
+    ["Lunch", "Hello"],
+    ["hello", "Hello"]
+  ];
 
   List<Widget> botMessage(context) {
-    return <Widget>[
-      Container(
-        margin: const EdgeInsets.only(right: 10.0),
-        child: CircleAvatar(
-          child:
-              // Padding(
-              //   padding: const EdgeInsets.all(5),
-              //child:
-              Image.asset('assets/images/InstaYum_chatbot.png'),
-          // ),
-          backgroundColor: Colors.white,
-          radius: 30,
+    // bring_recipes is a word saved in dialogflow that indicate we need to represent the resipes
+    if (text != "bring_recipes") {
+      return <Widget>[
+        Container(
+          margin: const EdgeInsets.only(right: 10.0),
+          child: CircleAvatar(
+            child:
+                // Padding(
+                //   padding: const EdgeInsets.all(5),
+                //child:
+                Image.asset('assets/images/InstaYum_chatbot.png'),
+            // ),
+            backgroundColor: Colors.white,
+            radius: 30,
+          ),
         ),
-      ),
-      Expanded(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
 //            Text(this.name,
 //                style: TextStyle(fontWeight: FontWeight.bold)),
-            Card(
-                elevation: 3,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20))),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    text,
-                  ),
-                )),
-          ],
+              Card(
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      text,
+                    ),
+                  )),
+            ],
+          ),
         ),
-      ),
-    ];
+      ];
+    } else {
+      return <Widget>[
+        Column(children: [
+          Card(
+            child: Container(
+              height: 90,
+              width: 330,
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 2.0),
+                      child: Text(
+                        "Keto diet plan",
+                        style: TextStyle(fontSize: 17, color: Colors.white),
+                      ),
+                    ),
+                    ElevatedButton(
+                      child: Text("Make it my current plan"),
+                      style: ElevatedButton.styleFrom(
+                        onPrimary: Colors.white,
+                        primary: Color(0xFFeb6d44),
+                      ),
+                      onPressed: () {
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(builder: (context) => MainPages()),
+                        // );
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => new RecipeView(
+                                    //key,
+                                    // autherName,
+                                    // autherImage,
+                                    "EiND3Sc8zCQginID4aTvVFwHE7n2",
+                                    "01f93d17-e5a1-4f0f-b8ba-ab985dc5a305",
+                                    "recipeName",
+                                    "https://firebasestorage.googleapis.com/v0/b/instayum-f7a34.appspot.com/o/recpie_image%2FdefaultRecipeImage.png?alt=media&token=f12725db-646b-4692-9ccf-131a99667e43",
+                                    "Breakfast",
+                                    "Appetizers",
+                                    "American",
+                                    ingredients,
+                                    dirctions,
+                                    dircimageUrlst)));
+                      },
+                    ),
+                  ]),
+            ),
+            // change z-axis place of card
+            elevation: 3,
+            shadowColor: Colors.black,
+            margin: EdgeInsets.all(20),
+            shape: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Colors.white)),
+            color: Colors.orangeAccent,
+          ),
+          Card(
+            child: Container(
+              height: 90,
+              width: 330,
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 2.0),
+                      child: Text(
+                        "Keto diet plan",
+                        style: TextStyle(fontSize: 17, color: Colors.white),
+                      ),
+                    ),
+                    ElevatedButton(
+                      child: Text("Make it my current plan"),
+                      style: ElevatedButton.styleFrom(
+                        onPrimary: Colors.white,
+                        primary: Color(0xFFeb6d44),
+                      ),
+                      onPressed: () {
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(builder: (context) => MainPages()),
+                        // );
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => new RecipeView(
+                                    //key,
+                                    // autherName,
+                                    // autherImage,
+                                    "EiND3Sc8zCQginID4aTvVFwHE7n2",
+                                    "01f93d17-e5a1-4f0f-b8ba-ab985dc5a305",
+                                    "recipeName",
+                                    "https://firebasestorage.googleapis.com/v0/b/instayum-f7a34.appspot.com/o/recpie_image%2FdefaultRecipeImage.png?alt=media&token=f12725db-646b-4692-9ccf-131a99667e43",
+                                    "Breakfast",
+                                    "Appetizers",
+                                    "American",
+                                    ingredients,
+                                    dirctions,
+                                    dircimageUrlst)));
+                      },
+                    ),
+                  ]),
+            ),
+            // change z-axis place of card
+            elevation: 3,
+            shadowColor: Colors.black,
+            margin: EdgeInsets.all(20),
+            shape: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Colors.white)),
+            color: Colors.orangeAccent,
+          )
+        ])
+      ];
+    }
   }
 
   List<Widget> userMessage(context) {
